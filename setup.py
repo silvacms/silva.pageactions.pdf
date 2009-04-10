cc11b3829e419897be9892f7877da2fb7cdf0e74
Sylvain Viollon <thefunny@gmail.com>
2009-04-10 09:33:55 +0000

    Page action to get the current page as a PDF.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='silva.pageactions.pdf',
+      version=version,
+      description="Create a PDF out of the current page in Silva.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva page actions pdf',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva', 'silva.pageactions'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'silva.core.views',
+        'silva.pageactions.base',
+        ],
+      )
