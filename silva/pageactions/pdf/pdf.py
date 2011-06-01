b7220bfb9b3b47a1913d8dd79863a783e6130c87
Sylvain Viollon <thefunny@gmail.com>
2011-06-01 17:35:31 +0200

    Change popen to subprocess to remove deprecation warning with Python 2.6.

--- silva/pageactions/pdf/pdf.py
+++ silva/pageactions/pdf/pdf.py
@@ -3,7 +3,7 @@
 # See also LICENSE.txt
 # $Id$
 
-import popen2
+import subprocess
 
 from five import grok
 from silva.core.views import views as silvaviews
@@ -29,25 +29,18 @@ class PDFPage(silvaviews.View):
     def pdf(self):
         """Convert the current page as a PDF.
         """
-
         print_html = component.getMultiAdapter(
-            (self.context, self.request), name='print.html')()
-        print_html = print_html.encode('cp1252', 'replace')
-        (stdout, stdin) = popen2.popen2((
+            (self.context, self.request), name='print.html')().encode('cp1252', 'replace')
+        command = subprocess.Popen(
             """htmldoc --charset cp-1252 --header . --fontsize 10 """
-            """--bodyfont helvetica --webpage -t pdf --quiet --jpeg - """))
-
-        stdin.write(print_html)
-        stdin.close()
-
-        pdf = stdout.read()
-        stdout.close()
+            """--bodyfont helvetica --webpage -t pdf --quiet --jpeg - """,
+            shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        pdf, error = command.communicate(input=print_html)
         return pdf
 
     def render(self):
         """Converts a HTML-Page to a PDF-Document.
         """
-
         pdf = self.pdf()
         self.HEAD()
         return pdf
